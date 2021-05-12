56c8360f3258d0b29a5a124c2649c755445356a5
PuneetGopinath <76863199+PuneetGopinath@users.noreply.github.com>
2021-05-12 19:37:25 +0530

    run format

--- index.js
+++ index.js
@@ -249,7 +249,9 @@ Toolkit.run(
     );
 
     if (!content.length) {
-      tools.exit.success("No PullRequest/Issue/IssueComment events found. Leaving readme unchanged.");
+      tools.exit.success(
+        "No PullRequest/Issue/IssueComment events found. Leaving readme unchanged."
+      );
     }
 
     if (content.length < MAX_LINES) {
