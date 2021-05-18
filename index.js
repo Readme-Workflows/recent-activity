4f77b014feefdc48ea255cd3c1ea309d59b6afe1
PuneetGopinath <76863199+PuneetGopinath@users.noreply.github.com>
2021-05-18 19:02:25 +0530

    Fix bug

--- index.js
+++ index.js
@@ -109,27 +109,33 @@ const to2Digit = (entity) => {
 };
 
 const makeCustomUrl = (item, type) => {
-  let url = Object.hasOwnProperty.call(item.payload, "issue")
-    ? `[` +
-        URL_TEXT.replace(/{ID}/g, `#${item.payload.issue.number}`).replace(
+    let url;
+    switch (type.toLowerCase()) {
+        case "issue_open":
+        case "issue_close":
+            url = `[` + URL_TEXT.replace(/{ID}/g, `#${item.payload.issue.number}`).replace(
           /{REPO}/g,
           item.repo.name
-        ) +
-        `](${urlPrefix}/${item.repo.name}/issues/${item.payload.issue.number})`
-    : `[` +
-        URL_TEXT.replace(
+        ) + `](${urlPrefix}/${item.repo.name}/issues/${item.payload.issue.number})`;
+            break;
+        case "comment":
+            `[` + URL_TEXT.replace(/{ID}/g, `#${item.payload.issue.number}`).replace(
+          /{REPO}/g,
+          item.repo.name
+        ) + `](${item.payload.comment.html_url})`;
+            break;
+        case "pr_open":
+        case "pr_close":
+        case "pr_merge":
+            url = `[` + URL_TEXT.replace(
           /{ID}/g,
           `#${item.payload.pull_request.number}`
-        ).replace(/{REPO}/g, item.repo.name) +
-        `](${urlPrefix}/${item.repo.name}/pull/${item.payload.pull_request.number})`;
-  if (type === "comment") {
-    url = `[` +
-        URL_TEXT.replace(/{ID}/g, `#${item.payload.issue.number}`).replace(
-          /{REPO}/g,
-          item.repo.name
-        ) +
-        `](${item.payload.comment.html_url})`
-  }
+        ).replace(/{REPO}/g, item.repo.name) + `](${urlPrefix}/${item.repo.name}/pull/${item.payload.pull_request.number})`;
+            break;
+        default:
+            tools.exit.failure("Failed while creating the url string.")
+            break;
+    }
   return url;
 };
 
