1c2d57b5b79390943e1855e78f990d1f04f442e1
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-05-19 21:46:29 +0530

    separated core inputs

--- index.js
+++ index.js
@@ -3,31 +3,11 @@
  * Copyright (c) 2021 Abhishek Joshi and Puneet Gopinath
  */
 
-const core = require("@actions/core");
 const fs = require("fs");
 const path = require("path");
 const { spawn } = require("child_process");
 const { Toolkit } = require("actions-toolkit");
 var dateFormat = require("dateformat");
-
-// Get config inputs
-const GH_USERNAME = core.getInput("GH_USERNAME");
-const COMMIT_MSG = core.getInput("COMMIT_MSG");
-const MAX_LINES = core.getInput("MAX_LINES");
-const README_FILE = core.getInput("README_FILE");
-const COMMENTS_ACTIVITY = core.getInput("COMMENTS_ACTIVITY");
-const ISSUE_OPENED = core.getInput("ISSUE_OPENED");
-const ISSUE_CLOSED = core.getInput("ISSUE_CLOSED");
-const PR_OPENED = core.getInput("PR_OPENED");
-const PR_CLOSED = core.getInput("PR_CLOSED");
-const PR_MERGED = core.getInput("PR_MERGED");
-const URL_TEXT = core.getInput("URL_TEXT");
-const TIMEZONE_OFFSET = core.getInput("TIMEZONE_OFFSET");
-const DATE_STRING = core.getInput("DATE_STRING");
-const DATE_FORMAT = core.getInput("DATE_FORMAT");
-
-let DISABLE_EVENTS = core.getInput("DISABLE_EVENTS").toLowerCase().split(",");
-DISABLE_EVENTS = DISABLE_EVENTS.map((event) => event.trim());
 
 /**
  * Returns the sentence case representation
