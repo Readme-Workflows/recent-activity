72be1701c144c60201a6e18349cecc17008b918b
Puneet Gopinath <baalkrshna@gmail.com>
2021-05-21 09:32:49 +0530

    Update filterContent.js

--- functions/filterContent.js
+++ functions/filterContent.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2020 James George
+ * Copyright (c) 2021 The Readme-Workflows organisation
+ */
+
 const serializers = require("../serializers");
 const { MAX_LINES } = require("../config");
 
