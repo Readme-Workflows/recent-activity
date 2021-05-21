460e74b076dabd4a8f8ea015f81790c883b17231
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-05-22 00:27:58 +0530

    added CreateEvent

---
+++ events/CreateEvent.js
@@ -0,0 +1,16 @@
+const { CREATE_REPO } = require("./config");
+const makeCustomUrl = require("./functions/makeCustomUrl");
+const toUrlFormat = require("./functions/toUrlFormat");
+
+const CreateEvent = (item) => {
+  if (item.payload.ref_type === "repository") {
+    return CREATE_REPO.replace(
+      /{REPO}/g,
+      toUrlFormat(item.repo.name, "createrepo")
+    ).replace(/{URL}/g, makeCustomUrl(item, "createrepo"));
+  } else {
+    return "";
+  }
+};
+
+module.exports = CreateEvent;
