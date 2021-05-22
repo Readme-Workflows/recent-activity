af2944bf7d652763b64a8493c402314d6a00b60b
PuneetGopinath <76863199+PuneetGopinath@users.noreply.github.com>
2021-05-22 16:59:06 +0530

    Add parseYaml.js

---
+++ functions/parseYaml.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2021 The Readme-Workflows organisation and Contributors
+ */
+
+const yaml = require("js-yaml");
+const fs = require("fs");
+
+//const { config_file } = require("../config");
+
+const parseYaml = (file) => {
+  return config = yaml.load(fs.readFileSync(file, "utf8"));
+};
+
+module.exports = parseYaml;
