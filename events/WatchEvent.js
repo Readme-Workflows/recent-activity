22030354020a045f1abc3fd2fe2bb14a140abf4c
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-05-22 16:17:38 +0530

    fix WatchEvent

--- events/WatchEvent.js
+++ events/WatchEvent.js
@@ -3,7 +3,7 @@ const makeCustomUrl = require("../functions/makeCustomUrl");
 const toUrlFormat = require("../functions/toUrlFormat");
 
 const WatchEvent = (item) => {
-  if (item.payload.action === "published") {
+  if (item.payload.action === "started") {
     return STAR.replace(/{REPO}/g, toUrlFormat(item.repo.name, "star")).replace(
       /{URL}/g,
       makeCustomUrl(item, "star")
