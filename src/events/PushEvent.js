29694f685f8b6f40617f4f4f8058cc7ffda38aba
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-08 13:47:58 +0530

    fix: log PushEvent payload for debugging
    we found this log: ⬆️ Pushed undefined commit(s) to [correct repo](correct url)

--- src/events/PushEvent.js
+++ src/events/PushEvent.js
@@ -2,6 +2,8 @@ const { push } = require("../config");
 const toUrlFormat = require("../functions/toUrlFormat");
 
 const PushEvent = (item) => {
+  console.log("PushEvent payload");
+  console.log(JSON.stringify(item.payload, null, 2));
   return push
     .replace(/{AMOUNT}/g, item.payload.size)
     .replace(/{REPO}/g, toUrlFormat(item.repo.name, "push"));
