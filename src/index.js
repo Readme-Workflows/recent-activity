c4de24d0d63a9dc40b1b068d897159649dff1c53
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-08 14:45:44 +0530

    refactor: use @actions/github instead of actions-toolkit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@
  */
 
 const fs = require("fs");
-const { Toolkit } = require("actions-toolkit");
+const github = require("@actions/github");
+const core = require("@actions/core");
+
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
 
 // configuration
 const { username, readme_file, max_lines, line_prefix } = require("./config");
@@ -17,109 +20,74 @@ const filterContent = require("./functions/filterContent");
 // accepted events
 const serializers = require("./serializers");
 
-Toolkit.run(
-  async (tools) => {
-    // Get the user's public events
-    tools.log.debug(`Getting activity for ${username}`);
-    const events = await tools.github.activity.listPublicEventsForUser({
-      username: username,
-      per_page: 100,
-    });
-    tools.log.debug(`${events.data.length} events found for ${username}.`);
+async function run() {
+  // Get the user's public events
+  core.debug(`Getting activity for ${username}`);
+  const { data: events } = await octokit.rest.activity.listPublicEventsForUser({
+    username: username,
+    per_page: 100,
+  });
 
-    let eventData = events.data
-      // Filter out any boring activity
-      .filter((event) => serializers.hasOwnProperty(event.type));
+  core.debug(`${events.data.length} events found for ${username}.`);
 
-    let content = filterContent(eventData);
+  let eventData = events.data
+    // Filter out any boring activity
+    .filter((event) => serializers.hasOwnProperty(event.type));
 
-    let readmeContent;
+  let content = filterContent(eventData);
 
-    try {
-      readmeContent = fs.readFileSync(readme_file, "utf-8").split("\n");
-    } catch (err) {
-      return tools.exit.failure(`Couldn't find the file named ${readme_file}`);
-    }
+  let readmeContent;
 
-    // Find the index corresponding to <!--RECENT_ACTIVITY:start--> comment
-    let startIdx = readmeContent.findIndex(
-      (content) => content.trim() === "<!--RECENT_ACTIVITY:start-->"
+  try {
+    readmeContent = fs.readFileSync(readme_file, "utf-8").split("\n");
+  } catch (err) {
+    return core.setFailed(`Couldn't find the file named ${readme_file}`);
+  }
+
+  // Find the index corresponding to <!--RECENT_ACTIVITY:start--> comment
+  let startIdx = readmeContent.findIndex(
+    (content) => content.trim() === "<!--RECENT_ACTIVITY:start-->"
+  );
+
+  // Early return in case the <!--RECENT_ACTIVITY:start--> comment was not found
+  if (startIdx === -1) {
+    return core.setFailed(
+      "Couldn't find the <!--RECENT_ACTIVITY:start--> comment. Exiting!"
     );
+  }
 
-    // Early return in case the <!--RECENT_ACTIVITY:start--> comment was not found
-    if (startIdx === -1) {
-      return tools.exit.failure(
-        "Couldn't find the <!--RECENT_ACTIVITY:start--> comment. Exiting!"
-      );
-    }
+  // Find the index corresponding to <!--RECENT_ACTIVITY:end--> comment
+  const endIdx = readmeContent.findIndex(
+    (content) => content.trim() === "<!--RECENT_ACTIVITY:end-->"
+  );
 
-    // Find the index corresponding to <!--RECENT_ACTIVITY:end--> comment
-    const endIdx = readmeContent.findIndex(
-      (content) => content.trim() === "<!--RECENT_ACTIVITY:end-->"
-    );
+  if (!content.length) {
+    return core.setOutput("No events found. Leaving readme unchanged.");
+  }
 
-    if (!content.length) {
-      tools.exit.success("No events found. Leaving readme unchanged.");
-    }
+  if (content.length < max_lines) {
+    core.info(`Found less than ${max_lines} activities`);
+  }
 
-    if (content.length < max_lines) {
-      tools.log.info(`Found less than ${max_lines} activities`);
-    }
-
-    if (startIdx !== -1 && endIdx === -1) {
-      // Add one since the content needs to be inserted just after the initial comment
-      startIdx++;
-      content.forEach((line, idx) =>
-        readmeContent.splice(
-          startIdx + idx,
-          0,
-          `${line_prefix.replace(/{NUM}/g, idx + 1)}${line}<br>`
-        )
-      );
-
-      // Append <!--RECENT_ACTIVITY:end--> comment
-      readmeContent.splice(
-        startIdx + content.length,
-        0,
-        "<!--RECENT_ACTIVITY:end-->"
-      );
-
-      readmeContent = appendDate(readmeContent);
-
-      // Update README
-      fs.writeFileSync(readme_file, readmeContent.join("\n"));
-
-      // Commit to the remote repository
-      try {
-        await commitFile();
-      } catch (err) {
-        tools.log.debug("Something went wrong");
-        return tools.exit.failure(err);
-      }
-      tools.exit.success("Wrote to README");
-    }
-
+  if (startIdx !== -1 && endIdx === -1) {
+    // Add one since the content needs to be inserted just after the initial comment
     startIdx++;
-
-    // Recent GitHub Activity content between the comments
-    const readmeActivitySection = readmeContent.slice(startIdx, endIdx);
-    if (readmeActivitySection.length) {
-      // Remove existing recent activity lines
-      readmeContent.splice(startIdx, endIdx - startIdx);
-    }
-    content.some((line, idx) => {
-      // User doesn't have 5 public events
-      if (!line) {
-        return true;
-      }
+    content.forEach((line, idx) =>
       readmeContent.splice(
         startIdx + idx,
         0,
         `${line_prefix.replace(/{NUM}/g, idx + 1)}${line}<br>`
-      );
-    });
+      )
+    );
+
+    // Append <!--RECENT_ACTIVITY:end--> comment
+    readmeContent.splice(
+      startIdx + content.length,
+      0,
+      "<!--RECENT_ACTIVITY:end-->"
+    );
+
     readmeContent = appendDate(readmeContent);
-    tools.log.success("Updated README with the recent activity");
 
     // Update README
     fs.writeFileSync(readme_file, readmeContent.join("\n"));
@@ -128,13 +96,45 @@ Toolkit.run(
     try {
       await commitFile();
     } catch (err) {
-      tools.log.debug("Something went wrong");
-      return tools.exit.failure(err);
+      core.debug("Something went wrong");
+      return core.setFailed(err);
     }
-    tools.exit.success("Pushed to remote repository");
-  },
-  {
-    event: ["schedule", "workflow_dispatch"],
-    secrets: ["GITHUB_TOKEN"],
+    core.info("Wrote to README");
   }
-);
+
+  startIdx++;
+
+  // Recent GitHub Activity content between the comments
+  const readmeActivitySection = readmeContent.slice(startIdx, endIdx);
+  if (readmeActivitySection.length) {
+    // Remove existing recent activity lines
+    readmeContent.splice(startIdx, endIdx - startIdx);
+  }
+  content.some((line, idx) => {
+    // User doesn't have 5 public events
+    if (!line) {
+      return true;
+    }
+    readmeContent.splice(
+      startIdx + idx,
+      0,
+      `${line_prefix.replace(/{NUM}/g, idx + 1)}${line}<br>`
+    );
+  });
+  readmeContent = appendDate(readmeContent);
+  core.info("Updated README with the recent activity");
+
+  // Update README
+  fs.writeFileSync(readme_file, readmeContent.join("\n"));
+
+  // Commit to the remote repository
+  try {
+    await commitFile();
+  } catch (err) {
+    core.debug("Something went wrong");
+    return core.setFailed(err);
+  }
+  core.info("Pushed to remote repository");
+}
+
+run();
